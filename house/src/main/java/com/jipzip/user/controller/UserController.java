3bdf4fb4fc10273a6dc2093b3497a65690daeb18
Hugi <81164267+Huggy2646@users.noreply.github.com>
2024-06-24 22:56:31 +0900

    Update UserController.java

--- house/src/main/java/com/jipzip/user/controller/UserController.java
+++ house/src/main/java/com/jipzip/user/controller/UserController.java
@@ -1,33 +1,33 @@
-package com.jipzip.user.controller;
+// package com.jipzip.user.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-import com.jipzip.user.model.service.UserService;
+// import com.jipzip.user.model.service.UserService;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.http.HttpSession;
 
-@Controller("/user")
-public class UserController {
+// @Controller("/user")
+// public class UserController {
 	
-	private UserService userservice;
+// 	private UserService userservice;
 
-	public UserController(UserService userservice) {
-		super();
-		this.userservice = userservice;
-	}
+// 	public UserController(UserService userservice) {
+// 		super();
+// 		this.userservice = userservice;
+// 	}
 	
-	// 로그아웃
-	@GetMapping("/logout")
-	private String logout(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		session.invalidate();
-		return "redirect:/index";
-	}
-
-	
+// 	// 로그아웃
+// 	@GetMapping("/logout")
+// 	private String logout(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
+// 		session.invalidate();
+// 		return "redirect:/index";
+// 	}
 
 	
+
 	
-}
+	
+// }
